602008d43a8172381203d2febc14e78bbb8adb58
Geass <geass1000@gmail.com>
2018-03-15 18:33:30 +0300

    delete: unused

--- src/units/custom/custom.module.ts
+++
@@ -1,5 +0,0 @@
-import { JalousieUnit } from './jalousie/jalousie.unit';
-
-export const CustomModule: Map<string, any> = new Map<string, any>([
-    [ 'Jalousie', JalousieUnit ],
-]);
--- src/units/native/native.module.ts
+++
@@ -1,7 +0,0 @@
-import { DeviceUnit } from './device/device.unit';
-import { BinaryValueUnit } from './binary-value/binary-value.unit';
-
-export const NativeModule: Map<string, any> = new Map<string, any>([
-    [ 'Device', DeviceUnit ],
-    [ 'BinaryValue', BinaryValueUnit ],
-]);
