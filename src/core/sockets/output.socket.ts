96b7e68a50ea8ff6b052b82b15ae13f6e62466f3
Geass <geass1000@gmail.com>
2018-03-16 17:20:10 +0300

    chore: add comments

--- src/core/sockets/output.socket.ts
+++ src/core/sockets/output.socket.ts
@@ -1,8 +1,10 @@
 import * as dgram from 'dgram';
 import * as _ from 'lodash';
+import * as Bluebird from 'bluebird';
+
 import { logger } from '../utils';
 
-import * as Bluebird from 'bluebird';
+import { IBACnetAddressInfo } from '../interfaces';
 
 export class OutputSocket {
     public className: string = 'OutputSocket';
@@ -12,6 +14,13 @@ export class OutputSocket {
         private address: string) {
     }
 
+    /**
+     * send - sends the message by unicast channel.
+     *
+     * @param  {Buffer} msg - message (bytes)
+     * @param  {string} reqMethodName - name of the BACnet service
+     * @return {Bluebird<any>}
+     */
     public send (msg: Buffer, reqMethodName: string): Bluebird<any> {
         const ucAddress = this.address;
         const ucPort = this.port;
@@ -27,6 +36,13 @@ export class OutputSocket {
         });
     }
 
+    /**
+     * sendBroadcast - sends the message by broadcast channel.
+     *
+     * @param  {Buffer} msg - message (bytes)
+     * @param  {string} reqMethodName - name of the BACnet service
+     * @return {Bluebird<any>}
+     */
     public sendBroadcast (msg: Buffer, reqMethodName: string): Bluebird<any> {
         this.app.setBroadcast(true);
         const bcAddress = '255.255.255.255';
@@ -44,8 +60,18 @@ export class OutputSocket {
         });
     }
 
+    /**
+     * logSendMethods - logs the "send" methods.
+     *
+     * @param  {string} address - address of the BACnet device
+     * @param  {number} port - port of the BACnet device
+     * @param  {Buffer} msg - message (bytes)
+     * @param  {string} sendMethodName - name of "send" method
+     * @param  {string} reqMethodName - name of the BACnet service
+     * @return {void}
+     */
     private logSendMethods (address: string, port: number, msg: Buffer,
-            sendMethodName: string, reqMethodName: string) {
+            sendMethodName: string, reqMethodName: string): void {
         try {
             logger.debug(`${this.className} - ${sendMethodName} (${address}:${port}): ${reqMethodName}`);
             logger.debug(`${this.className} - ${sendMethodName} (${address}:${port}): ${msg.toString('hex')}`);
