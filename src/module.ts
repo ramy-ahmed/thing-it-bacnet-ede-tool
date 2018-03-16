73f940b8690d3e4e3549710179249b6d024e7b87
Geass <geass1000@gmail.com>
2018-03-16 16:24:06 +0300

    change: update application configuration

--- src/module.ts
+++ src/module.ts
@@ -1,68 +1,15 @@
 import {
-    IBACnetModule,
+    IAppConfig,
 } from './core/interfaces';
 
-export const BACnetModule: IBACnetModule = {
+export const appConfig: IAppConfig = {
     port: 1235,
-    device: {
-        id: 9999,
-        vendorId: 123,
-        props: {
-            objectName: {
-                value: '[thing-it] Test Device Name',
-            },
-            description: {
-                value: '[thing-it] Test Device Description',
-            },
-            vendorName: {
-                value: 'THING TECHNOLOGIES GmbH Test',
-            },
-            modelName: {
-                value: '[thing-it] BACnet Test Server',
-            },
-        },
+    ede: {
+        header: {
+            projectName: 'Thing-it',
+            authorOfLastChange: 'Andrey',
+            versionOfRefFile: 1,
+            versionOfLayout: 2,
+        }
     },
-    units: [
-        {
-            name: 'BinaryValue',
-            id: 28,
-            props: {
-                presentValue: {
-                    value: 0,
-                },
-                statusFlags: {
-                    inAlarm: false,
-                    fault: true,
-                }
-            },
-        },
-        {
-            name: 'Jalousie',
-            units: [
-                {
-                    id: 25,
-                    alias: 'angle',
-                    props: {
-                        presentValue: {
-                            value: 23,
-                        },
-                        statusFlags: {
-                            inAlarm: false,
-                            fault: true,
-                        }
-                    }
-                },
-                {
-                    id: 26,
-                    alias: 'height',
-                    props: {
-                        statusFlags: {
-                            inAlarm: false,
-                            fault: true,
-                        }
-                    }
-                },
-            ],
-        },
-    ],
 }
