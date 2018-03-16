32fc78658db52b226f15f4c654b123a58c463ca3
Geass <geass1000@gmail.com>
2018-03-16 09:21:07 +0300

    add: "readProperty" method to the confirmed service

--- src/services/complex-ack.service.ts
+++ src/services/complex-ack.service.ts
@@ -3,7 +3,7 @@ import {
     BLVCFunction,
 } from '../core/enums';
 
-import { RequestSocket, ResponseSocket } from '../core/sockets';
+import { RequestSocket, OutputSocket } from '../core/sockets';
 
 import { complexACKPDU } from '../core/layers/apdus';
 import { blvc, npdu } from '../core/layers';
@@ -11,61 +11,6 @@ import { blvc, npdu } from '../core/layers';
 import { BACnetWriterUtil } from '../core/utils';
 
 export class ComplexACKService {
-
-    /**
-     * readProperty - handles the "readProperty" confirmed request.
-     *
-     * @param  {RequestSocket} req - request object (socket)
-     * @param  {ResponseSocket} resp - response object (socket)
-     * @return {type}
-     */
-    public readProperty (req: RequestSocket, resp: ResponseSocket) {
-        const invokeId = req.apdu.get('invokeId');
-        const apduService = req.apdu.get('service');
-
-        // Get object identifier
-        const objIdent = apduService.get('objIdent');
-        const objIdentValue = objIdent.get('value');
-        const objType = objIdentValue.get('type');
-        const objInst = objIdentValue.get('instance');
-
-        // Get property identifier
-        const propIdent = apduService.get('propIdent');
-        const propIdentValue = propIdent.get('value');
-
-        // Get BACnet property (for BACnet object)
-        const bnProp = req.unitManager.getUnitProperty(objInst, objType, propIdentValue);
-
-        // Generate APDU writer
-        const writerComplexACK = complexACKPDU.writeReq({
-            invokeId: invokeId
-        });
-        const writerReadProperty = complexACKPDU.writeReadProperty({
-            objInst: objInst,
-            objType: objType,
-            propId: bnProp.id,
-            propType: bnProp.type,
-            propValue: bnProp.values,
-        });
-        const writerAPDU = BACnetWriterUtil.concat(writerComplexACK, writerReadProperty);
-
-        // Generate NPDU writer
-        const writerNPDU = npdu.writeNPDULayer({});
-
-        // Generate BLVC writer
-        const writerBLVC = blvc.writeBLVCLayer({
-            func: BLVCFunction.originalUnicastNPDU,
-            npdu: writerNPDU,
-            apdu: writerAPDU,
-        });
-
-        // Concat messages
-        const writerBACnet = BACnetWriterUtil.concat(writerBLVC, writerNPDU, writerAPDU);
-
-        // Get and send BACnet message
-        const msgBACnet = writerBACnet.getBuffer();
-        return resp.send(msgBACnet, 'readProperty');
-    }
 }
 
 export const complexACKService: ComplexACKService = new ComplexACKService();
--- src/services/confirm-req.service.ts
+++ src/services/confirm-req.service.ts
@@ -1,10 +1,51 @@
 import {
     BACnetServiceTypes,
+    BLVCFunction,
 } from '../core/enums';
 
-import { RequestSocket, ResponseSocket } from '../core/sockets';
+import { confirmReqPDU } from '../core/layers/apdus';
+import { blvc, npdu } from '../core/layers';
+
+import { BACnetWriterUtil } from '../core/utils';
+
+import {
+    IConfirmedReqReadPropertyOptions,
+} from '../core/interfaces';
+
+import { RequestSocket, OutputSocket } from '../core/sockets';
 
 export class ConfirmReqService {
+
+    /**
+     * readProperty - sends the "readProperty" confirmed request.
+     *
+     * @param  {RequestSocket} req - request object (socket)
+     * @param  {OutputSocket} resp - response object (socket)
+     * @return {type}
+     */
+    public readProperty (opts: IConfirmedReqReadPropertyOptions, output: OutputSocket) {
+        // Generate APDU writer
+        const writerComplexACK = confirmReqPDU.writeReq(opts);
+        const writerReadProperty = confirmReqPDU.writeReadProperty(opts);
+        const writerAPDU = BACnetWriterUtil.concat(writerComplexACK, writerReadProperty);
+
+        // Generate NPDU writer
+        const writerNPDU = npdu.writeNPDULayer({});
+
+        // Generate BLVC writer
+        const writerBLVC = blvc.writeBLVCLayer({
+            func: BLVCFunction.originalUnicastNPDU,
+            npdu: writerNPDU,
+            apdu: writerAPDU,
+        });
+
+        // Concat messages
+        const writerBACnet = BACnetWriterUtil.concat(writerBLVC, writerNPDU, writerAPDU);
+
+        // Get and send BACnet message
+        const msgBACnet = writerBACnet.getBuffer();
+        return output.send(msgBACnet, 'readProperty');
+    }
 }
 
 export const confirmReqService: ConfirmReqService = new ConfirmReqService();
