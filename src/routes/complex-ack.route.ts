2bd91d1167754599058daa516448a3ac933723c9
Geass <geass1000@gmail.com>
2018-03-20 15:52:56 +0300

    change: move the readProperty routes to the separate router file

--- src/routes/complex-ack.route.ts
+++ src/routes/complex-ack.route.ts
@@ -8,6 +8,7 @@ import {
 import { edeService } from '../services';
 
 import { InputSocket, OutputSocket, ServiceSocket } from '../core/sockets';
+import { ReadPropertyRouter } from './complex-ack/read-property.route';
 
 export function ComplexACKRouter (
         inputSoc: InputSocket, outputSoc: OutputSocket, serviceSocket: ServiceSocket) {
@@ -16,18 +17,8 @@ export function ComplexACKRouter (
 
     logger.debug(`MainRouter - Request Service: ${BACnetConfirmedService[serviceChoice]}`);
     switch (serviceChoice) {
-        case BACnetConfirmedService.ReadProperty: {
-            const serviceMap = apduMessage.get('service');
-            const propId = serviceMap.get('propIdent');
-            const propIdValue = propId.get('value');
-
-            switch (propIdValue) {
-                case BACnetPropIds.objectList:
-                    return edeService.readPropertyObjectList(inputSoc, outputSoc, serviceSocket);
-                default:
-                    return edeService.readPropertyAll(inputSoc, outputSoc, serviceSocket);
-            }
-        }
+        case BACnetConfirmedService.ReadProperty:
+            return ReadPropertyRouter(inputSoc, outputSoc, serviceSocket);
     }
     return;
 }
---
+++ src/routes/complex-ack/read-property.route.ts
@@ -0,0 +1,35 @@
+import { logger } from '../../core/utils';
+
+import {
+    BACnetConfirmedService,
+    BACnetPropIds,
+} from '../../core/enums';
+
+import { edeService } from '../../services';
+
+import { InputSocket, OutputSocket, ServiceSocket } from '../../core/sockets';
+
+export function ReadPropertyRouter (
+        inputSoc: InputSocket, outputSoc: OutputSocket, serviceSocket: ServiceSocket) {
+    const apduMessage = inputSoc.apdu;
+
+    const serviceMap = apduMessage.get('service');
+    const propId = serviceMap.get('propIdent');
+    const propIdValue = propId.get('value');
+
+    const propArrayIndex = serviceMap.get('propArrayIndex');
+    const propArrayIndexValue = propId.get('value');
+
+    switch (propIdValue) {
+        case BACnetPropIds.objectList: {
+            switch (propArrayIndexValue) {
+                case 0:
+                    return edeService.readPropertyObjectListLenght(inputSoc, outputSoc, serviceSocket);
+                default:
+                    return edeService.readPropertyObjectListItem(inputSoc, outputSoc, serviceSocket);
+            }
+        }
+        default:
+            return edeService.readPropertyAll(inputSoc, outputSoc, serviceSocket);
+    }
+}
