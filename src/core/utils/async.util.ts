8add2eeb340c2182a515f76bc8de0532e7d700f9
Geass <geass1000@gmail.com>
2018-03-22 15:59:37 +0300

    add: "copyFile" and "moveFile" methods

--- src/core/utils/async.util.ts
+++ src/core/utils/async.util.ts
@@ -1,5 +1,8 @@
 import * as Bluebird from 'bluebird';
 import * as _ from 'lodash';
+import * as fs from 'fs';
+
+import { ApiError } from '../errors';
 
 export class AsyncUtil {
 
@@ -7,6 +10,42 @@ export class AsyncUtil {
         return new Bluebird((resolve, reject) => {
             setTimeout(() => { resolve(null); }, timeout);
             _.isFunction(callback) && callback();
+        });
+    }
+
+    static moveFile (oldPath: string, newPath: string): Bluebird<any> {
+        return new Bluebird((resolve, reject) => {
+            fs.rename(oldPath, newPath, (error) => {
+                if (!error) {
+                    return resolve();
+                }
+                if (error.code === 'EXDEV') {
+                    AsyncUtil.copyFile(oldPath, newPath);
+                }
+                throw new ApiError(`AsyncUtil - moveFile: ${error}`);
+            });
+        });
+    }
+
+    static copyFile (oldPath: string, newPath: string): Bluebird<any> {
+        return new Bluebird((resolve, reject) => {
+            const readStream = fs.createReadStream(oldPath);
+            const writeStream = fs.createWriteStream(newPath);
+
+            readStream.on('error', (error) => {
+                throw new ApiError(`AsyncUtil - copyFile: ${error}`);
+            });
+            writeStream.on('error', (error) => {
+                throw new ApiError(`AsyncUtil - copyFile: ${error}`);
+            });
+
+            readStream.on('close', () => {
+                fs.unlink(oldPath, () => {
+                    resolve();
+                });
+            });
+
+            readStream.pipe(writeStream);
         });
     }
 }
