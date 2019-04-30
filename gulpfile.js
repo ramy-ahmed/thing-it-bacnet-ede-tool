c0cdfd3f06ee8de34ebc58f7be82bce6da00be56
Nikita Mikhaliuk <nikkita1992@mail.ru>
2019-04-30 16:20:53 +0300

    delete: unused dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp');
-const runSequence = require('run-sequence');
 const ts = require('gulp-typescript');
 const tslint = require('gulp-tslint');
 const mocha = require('gulp-mocha');
