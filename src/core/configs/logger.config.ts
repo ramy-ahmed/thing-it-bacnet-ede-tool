226c6d8a4c34435332e9b46d4809537e015b84ab
Nikita Mikhaliuk <nikkita1992@mail.ru>
2019-05-20 16:00:01 +0300

    add: separate log file for error messages

--- src/core/configs/logger.config.ts
+++ src/core/configs/logger.config.ts
@@ -9,11 +9,21 @@ export const LoggerConfig = {
             level: 'debug',
         }),
         new (winston.transports.File)({
+            name: 'all-file',
             filename: 'all-logs.log',
             maxFiles: 1,
             timestamp: false,
             colorize: false,
             level: 'debug',
+            options: { flags: 'w' },
+        }),
+        new (winston.transports.File)({
+            name: 'errors-file',
+            filename: 'all-errors.log',
+            maxFiles: 1,
+            timestamp: false,
+            colorize: false,
+            level: 'error',
             options: { flags: 'w' },
         }),
     ]
